0728d23f049466e024df2dec720cbe382319af65
BrendonAlc <brendon_rodrigo@hotmail.com>
2023-03-09 23:42:25 -0300

    Classe criada para comparar Titular de conta.

---
+++ bytebank/src/br/com/bytebank/teste/TitularDaContaComparator.java
@@ -0,0 +1,16 @@
+package br.com.bytebank.teste;
+
+import java.util.Comparator;
+
+import br.com.bytebank.banco.modelo.Conta;
+
+public class TitularDaContaComparator implements Comparator<Conta> {
+
+	@Override
+	public int compare(Conta c1, Conta c2) {
+
+		String nomeC1 = c1.getTitular().getNome();
+		String nomeC2 = c2.getTitular().getNome();
+		return nomeC1.compareTo(nomeC2);
+	}
+}
